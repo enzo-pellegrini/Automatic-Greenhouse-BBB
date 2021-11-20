82c024d36b5534a6f570db90ed2273fd1fb00eef
Francesco Palma <90145614+spid3rmn@users.noreply.github.com>
2021-11-20 17:36:46 +0100

    sensors done

--- sensors.js
+++ sensors.js
@@ -1,5 +1,39 @@
-exports.sensors = {
-  temperature: 1.0,
-  humidity: 32,
-  light: 50
+const { exec } = require("child_process");
+
+exports.readSensors = () => {
+    let temp;
+    let hum;
+    let bright;
+
+    exec("./executables/light_sensor.out", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        
+        bright = parseInt(stdout);
+    });
+
+    exec("./executables/temp_umidity.out", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+
+        [temp, hum] = stdout.split(" ").map(s => parseFloat(s));
+    });
+
+    return {
+        temperature: temp,
+        humidity: hum,
+        brightness: bright
+    };
 }
